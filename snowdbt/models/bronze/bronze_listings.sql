d232c80d1aa01a9f958b96ee51b04698742cad58
maneesh8123 <maneesh8123@gmail.com>
2026-01-14 08:42:22 +0530

    silver layer

--- snowdbt/models/bronze/bronze_listings.sql
+++ snowdbt/models/bronze/bronze_listings.sql
@@ -4,6 +4,6 @@
 SELECT
     *
 FROM {{ source('staging', 'listings') }}
-{% if incremental_flag == 1 %}
-WHERE {{ incremental_col }} > (SELECT COALESCE(MAX({{ incremental_col }}), '1900-01-01') FROM {{ ref('bronze_listings') }})
+{% if is_incremental() %}
+    WHERE CREATED_AT > (SELECT COALESCE(MAX(CREATED_AT), '1900-01-01') FROM {{ this }})
 {% endif %}
